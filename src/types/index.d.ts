1c62e22b7d66df33c93e4c2d690e798a67635e44
Vy Tran <ttv.thuyvy.1544@gmail.com>
2025-04-02 14:14:15 +0700

    setup assets

---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+interface Interview {
+  id: string;
+  type: string;
+  role: string;
+  level: string;
+  userId: string;
+  createdAt: string;
+  finalized: boolean;
+  questions: string[];
+  techstack: string[];
+}
